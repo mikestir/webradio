1f32d92f174432a7915ca97eb81f99afe4559dc6
Mike Stirling <opensource@mikestirling.co.uk>
2013-07-27 08:52:08 +0100

    Added readme, license to file headers

--- src/io/samplesource.h
+++ src/io/samplesource.h
@@ -4,6 +4,28 @@
  *  Created on: 24 Jun 2013
  *      Author: mike
  */
+/*
+ * WebRadio web-based Software Defined Radio
+ *
+ * Copyright (C) 2013 Mike Stirling
+ *
+ * This file is part of WebRadio (http://www.mike-stirling.com/webradio)
+ *
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef SAMPLESOURCE_H_
 #define SAMPLESOURCE_H_
--- src/web/audiostream.h
+++ src/web/audiostream.h
@@ -1,8 +1,24 @@
 /*
- * audiostream.h
+ * WebRadio web-based Software Defined Radio
  *
- *  Created on: 20 Jun 2013
- *      Author: mike
+ * Copyright (C) 2013 Mike Stirling
+ *
+ * This file is part of WebRadio (http://www.mike-stirling.com/webradio)
+ *
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef AUDIOSTREAM_H_
